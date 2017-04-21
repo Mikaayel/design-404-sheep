5f4740f9a5051e53f53595e9dd1e5b7cb6dac1ce
Mikaayel <mrehman@me.com>
2017-04-21 20:25:05 +0100

    updated tpography and colours

--- src/components/main.js
+++ src/components/main.js
@@ -4,8 +4,9 @@ let Main = React.createClass({
     render: function() {
         return (
             <div id="main--container">
-                <h1>Page Not Found</h1>
-                <h2>We couldn't find what you were looking for.</h2>           
+                <h1>404</h1>
+                <h2>It looks like you're lost...</h2>
+                <button>Go Back Home</button>         
             </div>
         );
     }
